3c6e1861c795fcbc7eaffb0d439c682b1fecb708
LabKitty <labkittyx@gmail.com>
2014-09-29 12:23:35 +0300

    Argumendita funktsioon

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>harjutus-9 - Funktsioonid ja nende defineerimine</title>
+</head>
+
+<body>
+	<h2>Funktsiooni defineerimine</h2>
+		<h3>Argumendita funktsioon</h3>
+			<?php
+				function say_hello() {
+					echo 'Minu esimene funktsioon!<br>';
+				}
+				say_hello();
+			?>
+		
+</body>
+</html>
